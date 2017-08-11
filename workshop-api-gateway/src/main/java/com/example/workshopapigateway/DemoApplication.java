6d9b5e96ab6df748003e28f9aef6924128ba86aa
Teng Li <otec.teng@gmail.com>
2017-08-11 14:13:12 +0800

    feat(oauth):4.3 add oauth with github

--- workshop-api-gateway/src/main/java/com/example/workshopapigateway/DemoApplication.java
+++ workshop-api-gateway/src/main/java/com/example/workshopapigateway/DemoApplication.java
@@ -2,19 +2,30 @@ package com.example.workshopapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @EnableZuulProxy
 @EnableFeignClients
 @SpringCloudApplication
-@EnableConfigurationProperties
-
-public class DemoApplication {
-
+//@EnableConfigurationProperties
+@EnableOAuth2Sso
+@RestController
+public class DemoApplication extends WebSecurityConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
 	}
 }
